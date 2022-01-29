3f10ef72075f3f9aa55f8ab32f951f80535fa7b9
saiful-dev <saiful.hstu.sii@gmail.com>
2022-01-29 19:59:33 +0600

    movement between component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
-import React from 'react';
+import React,{useState} from 'react';
 import Todo from './Components/Todo';
+import Header from './Components/Header';
+import Auth from './Components/Auth';
+
 function App() {
+  const [page,setPage]=useState('auth');
+
+  const switchPage= (pageName)=>{
+      setPage(pageName);
+  };
+
   return (
     <div className="App">
-          
-        <Todo/>
+        <Header  onLoadTodos={switchPage.bind(this,'todos')} onLoadAuth={switchPage.bind(this,'auth')}/>
+        <hr/>
+
+        {page ==='auth' ? <Auth/> :<Todo/>}
+        
+
+        
     </div>
   );
 }
---
+++ src/Components/Auth.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Auth=props=>(
+    <h1>Auth Component</h1>
+)
+
+export default Auth;
